4ee5ea4fd6a1f80cd911985da6d0c1b8f48a4c8f
Hasanain Mohammed <hasanain98@gmail.com>
2019-03-23 23:15:27 -0500

    server is working

---
+++ models/blood.js
@@ -0,0 +1,7 @@
+module.exports = function(sequelize, DataTypes) {
+  var Blood = sequelize.define("Blood", {
+    quantity: DataTypes.INTEGER,
+    bloodType: DataTypes.STRING
+  });
+  return Donor;
+};
---
+++ models/donor.js
@@ -0,0 +1,11 @@
+module.exports = function(sequelize, DataTypes) {
+  var Donor = sequelize.define("Donor", {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    gender: DataTypes.STRING,
+    contactNumber: DataTypes.STRING
+    bloodType: DataTypes.STRING
+  });
+  return Donor;
+};
