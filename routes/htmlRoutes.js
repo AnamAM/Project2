a82d2292e264b89d54f885376d981720a6c24877
Piper <skylarpiper710@gmail.com>
2019-03-25 13:26:02 -0500

    donor sign up page added in

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,15 +10,23 @@ module.exports = function(app) {
       });
     });
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/donor", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("donor", {
+        msg: "Welcome!",
+        examples: dbExamples
       });
     });
   });
+
+  // Load example page and pass in an example by id
+  // app.get("/donor/", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
