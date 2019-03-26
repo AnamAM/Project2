c8e19c2ba79195e05e8534aa6a4ec16427ce7340
Hasanain Mohammed <hasanain98@gmail.com>
2019-03-25 19:42:15 -0500

    check point

--- routes/donor-Api.js
+++ routes/donor-Api.js
@@ -1,17 +1,17 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/blood", function(req, res) {
+  app.get("/api/Donor", function(req, res) {
     db.Donor.findAll({
       include: [db.Blood]
-    }).then(function(dbBlood) {
-      res.json(dbBlood);
+    }).then(function(dbDonor) {
+      res.json(dbDonor);
     });
   });
   
-  app.post("/api/blood", function(req, res) {
-    db.Donor.create(req.body).then(function(dbBlood) {
-      res.json(dbBlood);
+  app.post("/api/Donor", function(req, res) {
+    db.Donor.create(req.body).then(function(dbDonor) {
+      res.json(dbDonor);
     });
   });
 };
