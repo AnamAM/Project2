0988f07a79eacdd0b77b471b303c237f9e41ce23
Anam Mohammad <m.anam0603@gmail.com>
2019-03-27 17:56:00 -0500

    pulling master

--- public/js/donor.js
+++ public/js/donor.js
@@ -1,15 +1,32 @@
-$(document).ready(function() {
-
-    $("#donor-submit").on("click", function(event) {
+$(document).ready(function () {
+    $("#donorSubmit").on("click", function (event) {
         event.preventDefault();
-
         var newDonor = {
-            firstName: $("fname").val().trim(),
-            lastName: $("lname").val().trim(),
-            age: $("age").val().trim(),
-            gender: $("gender").val().trim(),
-            contactNumber: $("contactNumber").val().trim(),
-            bloodType: $("bloodType").val().trim()
+            firstName: $("#fname").val(),
+            lastName: $("#lname").val().trim(),
+            gender: $("#gender").val().trim(),
+            age: parseInt($("#age").val().trim()),
+            contactNumber: $("#contactNumber").val().trim(),
+            bloodTypeID: $("#bloodType").val().trim()
+        };
+
+        $("#fname").val("");
+        $("#lname").val("");
+        $("#gender").val("");
+        $("#age").val("");
+        $("#contactNumber").val("");
+        $("#bloodType").val("");
+
+        console.log(newDonor)
+        $.ajax("/api/donor", {
+            type: "POST",
+            data: newDonor
+        }).then(function (req, res) {
+            console.log("created new Donor!!");
+            // Reload the page to get the updated list
+            // location.reload();
         }
-    });
-});
+
+        );
+    })
+})
